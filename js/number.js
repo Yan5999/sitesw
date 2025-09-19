58382f1ce39caf53959ee696abc3cc08fd0982ac
Yan <98166607+Yan5999@users.noreply.github.com>
2025-09-19 19:12:29 +0300

    1.1 add js file

--- js/number.js
+++ js/number.js
@@ -2,21 +2,20 @@ function animateCounter(id, target, duration = 2000) {
     const element = document.getElementById(id);
     const start = 0;
     const startTime = performance.now();
-  
-    function update(currentTime) {
-      const elapsed = currentTime - startTime;
-      const progress = Math.min(elapsed / duration, 1);
-      const value = Math.floor(progress * target);
-      element.textContent = value.toLocaleString(); 
-      if (progress < 1) {
-        requestAnimationFrame(update);
-      }
-    }
-  
-    requestAnimationFrame(update);
-  }
-  
 
-  document.addEventListener("DOMContentLoaded", function () {
-    animateCounter("counter", 265517237); 
-  });
+    function update(currentTime) {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / duration, 1);
+        const value = Math.floor(progress * target);
+        element.textContent = value.toLocaleString();
+        if (progress < 1) {
+            requestAnimationFrame(update);
+        }
+    }
+
+    requestAnimationFrame(update);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    animateCounter('counter', 265517237);
+});
